27c99217b6aaa31bf10b613b6afdf0bee0435bd0
LSKozarina <lubov.kozarina@pix.ru>
2025-04-22 23:13:51 +0300

    added TranstelematicaTests

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,47 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.baseUrl = "https://transtelematica.ru";
+        Configuration.timeout = 5000;
+
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "125.0");
+        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addAllureListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        Selenide.closeWebDriver();
+    }
+}
---
+++ src/test/java/tests/TranstelematicaTests.java
@@ -0,0 +1,69 @@
+package tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+import static io.qameta.allure.Allure.step;
+
+public class TranstelematicaTests extends TestBase {
+
+    @Test
+    @DisplayName("Проверка заголовка страницы")
+    void checkPageTitle() {
+        step("Открыть главную страницу", () -> open(baseUrl));
+        step("Проверить заголовок <title>", () ->
+                title().equals("Сервисный центр Транстелематика")
+        );
+    }
+
+    @Test
+    @DisplayName("Переход в раздел 'О компании'")
+    void goToAboutPage() {
+        step("Открыть главную страницу", () -> open(baseUrl));
+        step("Нажать на пункт меню 'О компании'", () ->
+                $("a[href='/about/']").click()
+        );
+        step("Проверить заголовок страницы 'О компании'", () ->
+                $("h1").shouldHave(text("О компании"))
+        );
+    }
+
+    @Test
+    @DisplayName("Переход в раздел 'Услуги'")
+    void goToServicesPage() {
+        step("Открыть главную страницу", () -> open(baseUrl));
+        step("Нажать на пункт меню 'Услуги'", () ->
+                $("a[href='/services/']").click()
+        );
+        step("Проверить заголовок страницы 'Услуги'", () ->
+                $("h1").shouldHave(text("Услуги"))
+        );
+    }
+
+    @Test
+    @DisplayName("Переход в раздел 'Проекты'")
+    void goToProjectsPage() {
+        step("Открыть главную страницу", () -> open(baseUrl));
+        step("Нажать на пункт меню 'Проекты'", () ->
+                $("a[href='/projects/']").click()
+        );
+        step("Проверить заголовок страницы 'Проекты'", () ->
+                $("h1").shouldHave(text("Проекты"))
+        );
+    }
+
+    @Test
+    @DisplayName("Переход в раздел 'Контакты'")
+    void goToContactsPage() {
+        step("Открыть главную страницу", () -> open(baseUrl));
+        step("Нажать на пункт меню 'Контакты'", () ->
+                $("a[href='/contacts/']").click()
+        );
+        step("Проверить заголовок страницы 'Контакты'", () ->
+                $("h1").shouldHave(text("Контакты"))
+        );
+    }
+}
